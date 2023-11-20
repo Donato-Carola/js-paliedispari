6321e688864f0a7f422a66ce54e30b47dd0570e4
Donato-Carola <don94@live.it>
2023-11-20 15:41:06 +0100

    primo esercizio, da rivedere e modificare il controllo su chi ha vinto

--- js/script.js
+++ js/script.js
@@ -10,3 +10,51 @@ Dichiariamo chi ha vinto.*/
 Creare una funzione per capire se la parola inserita è palindroma
 E' vietato usare split(), reverse() e simili.*/
 
+//! Faccio scegliere all'utente Pari o Dispari con il prompt e stamparlo in console
+
+const pariDispari=prompt('scegli pari o dispari')
+console.log(pariDispari);
+
+//! chiedo all'utente  un numero tra 1 e 5 e stamparlo in console.log
+const NumberUser = parseInt(prompt('scegli un numero tra 1 e 5'));
+console.log(NumberUser);
+
+//! Inizializzo la variabile pari a zero cosi da poter riutilizzarla per richiamre la funzione 
+let pcNumber=0
+
+//! creo la funzione per il numero random per il pc da 1a 5
+
+function randomNumber(randomNumberPc){
+ randomNumberPc = (Math.floor((Math.random(1)*5)+1));
+console.log(`il numero random è: ${randomNumberPc}`);
+return randomNumberPc
+}
+
+//! faccio la somma richiamando la funzione e sommandola al numero intero dell'utente
+
+somma = randomNumber(pcNumber) + NumberUser
+console.log(somma)
+
+
+//!inizializzo la variabile con stringa vuota per il risultato della funzione pari o dispari(DA RIVEDERE)
+let pariDispariGame = '';
+
+function sommaNumber(GameOver){
+    if(somma % 2 === 1){
+          console.log('dispari') 
+    }else{
+        console.log('pari')
+    }
+    return GameOver
+
+}
+
+sommaNumber(pariDispariGame)
+
+//!Faccio l'if per controllare chi ha vinto (DA RIVEDERE)
+
+if ((sommaNumber(pariDispariGame)) == pariDispari){
+    console.log('User win')
+}else{
+    console.log('Pc win')
+}
